11f9af5d3bf1d9a73acb7882e63b4e208799f8af
Muzamil Kaleem <muzamil.kaleem@arbisoft.com>
2026-09-25 14:24:09 +0500

    KAN-5: Add error handling and 404 handler to address review feedback

--- app.js
+++ app.js
@@ -6,4 +6,13 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to DevLake' });
 });
 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
 module.exports = app;
